643f38364c0f5cc49f47bb2cd4e2350d227567a3
FatimahAs <46461256+FatimahAs@users.noreply.github.com>
2024-10-25 21:33:46 +0300

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Fatimah</span>
+      <span>© Fatimah Dev Admin Dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
